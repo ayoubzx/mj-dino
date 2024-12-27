54f7e4736602db9b2062308cf97624b00bc218bf
ayoubzx <150842604+ayoubzx@users.noreply.github.com>
2024-12-27 23:25:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ import * as path from "node:path";
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Deno.env.get("PORT") || 3000;
+
 
 // قائمة مفاتيح API
 const apiKeys = [
